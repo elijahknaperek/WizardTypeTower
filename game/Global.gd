5d9100e6a55e2f8ef90f3aa1f73a36288d8c3c6a
Elijah <ElijahKnaperek@gmail.com>
2024-09-13 00:16:53 -0400

    changed char2

--- game/Global.gd
+++ game/Global.gd
@@ -6,7 +6,7 @@ var GAME_SCENE = load("res://game/tower_game.tscn")
 const char_lvl = ["abcdefghijklmnopqrstuvwxyz",
 				"abcdefghijklmnopqrstuvwxyz0123456789",]
 const chars = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-const chars2 = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy"
+const chars2 = "asdfjkl;ASDFJKL:!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy"
 
 
 var score
