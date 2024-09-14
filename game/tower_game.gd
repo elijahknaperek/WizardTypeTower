68ed279cbbf9f5c365d74775494a44a03e4aa218
Elijah <ElijahKnaperek@gmail.com>
2024-09-13 23:33:57 -0400

    batching

--- game/tower_game.gd
+++ game/tower_game.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 const enemy = preload("res://game/enemy.tscn")
 const GAME_OVER_SCENE = preload("res://game/game_over.tscn")
-var cpm = 30.0
+var wpm = 10.0:
+	set(v):
+		wpm = max(v,5.0)
 
 var shield_chance = 0.1
 
@@ -16,21 +18,23 @@ func _ready() -> void:
 	
 
 func spawn_enemy():
-	var e = enemy.instantiate()
-	
-	
-	e.global_position = ($Tower.global_position+Vector2(680,0).rotated(randf() * 2 * PI))
-	e.player = $Tower
-	if randf() < shield_chance:
-		e.shield = 1
-	e.dead.connect(on_enemy_defeat)
-	e.closest = !( $Enemies.get_child_count() > 0 )
-	$Enemies.add_child(e)
+	var num_to_spawn = int(clamp(randfn(4,3),1,min(12,wpm*0.5)))
+	var pos = randf() * 2 * PI
+	for total_spawned in num_to_spawn:
+		var e = enemy.instantiate()
+		e.global_position = ($Tower.global_position+Vector2(680 + 100 * total_spawned,0).rotated(randf() * 2 * PI))
+		e.player = $Tower
+		if randf() < shield_chance:
+			e.shield = 1
+		e.dead.connect(on_enemy_defeat)
+		e.closest = !( $Enemies.get_child_count() > 0 )
+		$Enemies.add_child(e)
+		
 	
 	
 	
 func on_enemy_defeat(bonus):
-	cpm = cpm + 0.1 + (0.1 * bonus)
+	wpm = wpm + 0.01 + (0.01 * bonus)
 	Global.score += 1 + bonus
 	Global.streak += 1
 	%Score.text = str(Global.score)
@@ -38,11 +42,13 @@ func on_enemy_defeat(bonus):
 
 func _on_spawn_timeout() -> void:
 	spawn_enemy()
-	$Spawn.wait_time = 60.0/cpm
+	$Spawn.wait_time = 60.0/wpm
+	$Spawn.start()
 
 
 func _on_tower_took_damage(hp: Variant) -> void:
 	%SegmentedProgressBar.value = hp/10.0
+	wpm -= 10.0
 	
 func _on_Tower_died():
 	if dead:
